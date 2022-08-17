a6c638f0dea3b97848ffeb32b98946bf88c10cd7
lilgoast <105431041+lilgoast@users.noreply.github.com>
2022-08-17 20:02:53 +0300

    Initial commit

---
+++ Assets/Scripts/CollisionHolder.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CollisionHolder : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        switch(other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Friendly");
+                break;
+            case "Finish":
+                Debug.Log("Finish");
+                break;
+            case "Fuel":
+                Debug.Log("Fuel");
+                break;
+            default:
+                Debug.Log("Lose");
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] int thrustSpeed = 100;
+    [SerializeField] int rotationSpeed = 100;
+    Rigidbody rb;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessTrust();
+        ProcessRotation();
+    }
+
+    void ProcessTrust()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            if(!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+            rb.AddRelativeForce(Vector3.up * thrustSpeed * Time.deltaTime);
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationSpeed);
+        }
+    }
+
+    public void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false;
+    }
+}
